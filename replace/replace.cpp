a8d69543fe59c5990c26cc6bf5fa988c4b3cfb40
Marcel Neidinger <neidinger@phaenovum.de>
2014-02-28 15:57:19 +0100

    replace initial commit

---
+++ replace/replace.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  replace.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  28.02.2014 15:56:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Marcel Neidinger (mn), neidinger@phaenovum.de
+ *        Company:  phaenovum Schülerforschungszentrum, Lörrach
+ *
+ * =====================================================================================
+ */
+
+
